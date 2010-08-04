865e81ff322ad1982a3efb53f015634b880faebe
Jinzhu <wosmvp@gmail.com>
2010-08-04 21:49:56 +0800

    add method feature, scenario, background

--- lib/tester.rb
+++ lib/tester.rb
@@ -4,3 +4,20 @@ require 'tester/table'
 
 module Tester
 end
+
+class MiniTest::Spec
+	def self.scenario desc, &block
+		self.it desc, &block
+	end
+
+	def self.background type = :each, &block
+		self.before type, &block
+	end
+end
+
+
+module Kernel
+	def feature desc, &block
+		describe desc, &block
+	end
+end
